cc533a355bc69d1cfd9b78b6fcd704995b71c25b
Murali <mkmurali.mca@gmail.com>
2023-07-07 20:42:20 +0530

    sweet alert enabled

--- src/app/postproperty/postproperty.component.ts
+++ src/app/postproperty/postproperty.component.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { postproperty } from '../class/postproerty';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 
 @Component({
@@ -13,25 +14,25 @@ import { postproperty } from '../class/postproerty';
   templateUrl: './postproperty.component.html',
   styleUrls: ['./postproperty.component.css']
 })
-export class PostpropertyComponent implements OnInit{
+export class PostpropertyComponent implements OnInit {
 
   // addproperty:postproperty={
   //   name:''
   // };
-  
-  selectedPropertyType: propertytype = new propertytype(1,'');
-  propertytypes: propertytype[] =[];
-  propertysubtypes: propertysubtype[] = [];
-  selectedSubType:any;
-  choosenpropertytypeid:any;
-  choosenpropertytypename:any;
-  file:any;
-  
 
-  PhotoFileName="anonymous.png";
-  PhotoPath=environment.PHOTO_URL;  
-  personName: any;
-  ownershipType: any;
+  selectedPropertyType: propertytype = new propertytype(1, '');
+  propertytypes: propertytype[] = [];
+  propertysubtypes: propertysubtype[] = [];
+  selectedSubType: any;
+  choosenpropertytypeid: any;
+  choosenpropertytypename: any;
+  file: any;
+
+
+  PhotoFileName = "anonymous.png";
+  PhotoPath = environment.PHOTO_URL;
+  personName: string | undefined;
+  ownershipType: string |undefined;
   personAddress: any;
   street: any;
   city: any;
@@ -48,61 +49,90 @@ export class PostpropertyComponent implements OnInit{
   propertyDescription: any;
   mapURL: any;
   imageFileName: any;
-  dimensions:any;
+  dimensions: any;
 
-  constructor(private typeService: TypeService,private http:HttpClient ) { }
+  constructor(private typeService: TypeService, private http: HttpClient) { }
 
   ngOnInit() {
     this.propertytypes = this.typeService.getpropertytypes();
-    this.onSelect(this.selectedPropertyType.id);    
+    this.onSelect(this.selectedPropertyType.id);
   }
 
-  onSelect(propertytypeid:any) {
+  onSelect(propertytypeid: any) {
     this.propertysubtypes = this.typeService.getpropertysubtypes().filter((item) => item.propertytypeid == propertytypeid);
   }
 
-  imageUpload(event:any){
-    this.file=event.target.files[0];
-    const formData:FormData=new FormData();
-    formData.append('file',this.file, this.file.name);
-    
-    
-    this.http.post(environment.API_URL+'Properties/SaveFile',formData)
-    .subscribe((data:any)=>{
-      this.PhotoFileName=data.toString();    
-    });
+  imageUpload(event: any) {
+    this.file = event.target.files[0];
+    const formData: FormData = new FormData();
+    formData.append('file', this.file, this.file.name);
+
+
+    this.http.post(environment.API_URL + 'Properties/SaveFile', formData)
+      .subscribe((data: any) => {
+        this.PhotoFileName = data.toString();
+      });
   }
 
-  submitForm(){
-    this.choosenpropertytypeid = this.selectedPropertyType.id-1;
-    this.choosenpropertytypename = this.propertytypes[this.choosenpropertytypeid].name;
+  validateSubmitForm(): boolean {
+    if(this.personName == undefined || this.personName.length==0){
+      Swal.fire({
+        title: 'Warning!',
+        text: "Person Name is Mandatory",
+        icon: 'warning'        
+      });
+      return false;
+    }
+    else if(this.ownershipType == undefined || this.ownershipType.length==0)
+    {
+      Swal.fire({
+        title: 'Warning!',
+        text: "Ownership Type is Mandatory",
+        icon: 'warning'        
+      });      
+      return false;
+    }
+    return true;
+  }
 
-    var val = {
-      personName : this.personName,
-      ownershipType : this.ownershipType,
-      personAddress:this.personAddress,
-      street:this.street,
-      city:this.city,
-      state:this.state,
-      zip:this.zip,
-      contactNumber:this.contactNumber,
-      emailAddress:this.emailAddress,
-      purpose:this.purpose,
-      location:this.location,
-      propertyType:this.choosenpropertytypename,
-      propertySubtype:this.propertySubtype,
-      price:this.price,
-      propertytitle:this.propertytitle,
-      propertyDescription:this.propertyDescription,
-      mapURL:this.mapURL,
-      imageFileName:this.file.name,
-      dimensions:this.dimensions,
+  submitForm() {
 
-    };
-    console.log("value : "+ val);
-    console.log("Image Name" + val.imageFileName);
-    console.log("post property button clicked");
-    this.http.post(environment.API_URL+"Properties", val)
-    .subscribe(res => { alert("Data added Successfully.")});
+    debugger;    
+      this.choosenpropertytypeid = this.selectedPropertyType.id - 1;
+      this.choosenpropertytypename = this.propertytypes[this.choosenpropertytypeid].name;
+      if (this.validateSubmitForm()) {
+      var val = {
+        personName: this.personName,
+        ownershipType: this.ownershipType,
+        personAddress: this.personAddress,
+        street: this.street,
+        city: this.city,
+        state: this.state,
+        zip: this.zip,
+        contactNumber: this.contactNumber,
+        emailAddress: this.emailAddress,
+        purpose: this.purpose,
+        location: this.location,
+        propertyType: this.choosenpropertytypename,
+        propertySubtype: this.propertySubtype,
+        price: this.price,
+        propertytitle: this.propertytitle,
+        propertyDescription: this.propertyDescription,
+        mapURL: this.mapURL,
+        imageFileName: this.file.name,
+        dimensions: this.dimensions,
+
+      };
+
+      console.log("value : " + val);
+      console.log("Image Name" + val.imageFileName);
+      console.log("post property button clicked");
+      this.http.post(environment.API_URL + "Properties", val)
+        .subscribe(res => { Swal.fire({
+          title: 'Swati Real Estate',
+          text: "Your input added successfully!!!",
+          icon: 'success'        
+        });      });
+    }
   }
 }
