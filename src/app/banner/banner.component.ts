7e52e202b5207365c3da704f804ee989f6d0e761
Murali <mkmurali.mca@gmail.com>
2023-08-01 11:19:37 +0530

    Banner Fix

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare var $: any;
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
 })
 export class BannerComponent {
-  
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((ev: NavigationEnd) => {
+        console.log('test');
+        $.getScript('assets/js/flexslider-config.js');
+      });
+  }
 }
---
+++ src/assets/js/flexslider-config.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function () {
+  jQuery(".flexslider").each(function () {
+    var sliderInstance = jQuery(this);
+    console.log(sliderInstance);
+    (sliderAutoplay =
+      sliderInstance.attr("data-autoplay") == "yes" ? true : false),
+      (sliderPagination =
+        sliderInstance.attr("data-pagination") == "yes" ? true : false),
+      (sliderArrows =
+        sliderInstance.attr("data-arrows") == "yes" ? true : false),
+      (sliderDirection = sliderInstance.attr("data-direction")
+        ? sliderInstance.attr("data-direction")
+        : "horizontal"),
+      (sliderStyle = sliderInstance.attr("data-style")
+        ? sliderInstance.attr("data-style")
+        : "fade"),
+      (sliderSpeed = sliderInstance.attr("data-speed")
+        ? sliderInstance.attr("data-speed")
+        : "5000"),
+      (sliderPause = sliderInstance.attr("data-pause") == "yes" ? true : false),
+      sliderInstance.flexslider({
+        animation: sliderStyle,
+        easing: "swing",
+        direction: sliderDirection,
+        slideshow: sliderAutoplay,
+        slideshowSpeed: sliderSpeed,
+        animationSpeed: 600,
+        initDelay: 0,
+        randomize: false,
+        pauseOnHover: sliderPause,
+        controlNav: sliderPagination,
+        directionNav: sliderArrows,
+        prevText: "",
+        nextText: "",
+        // start: function () { jQuery(".flex-caption").fadeIn(); }
+      });
+  });
+});
